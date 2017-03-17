a3c9c652eed48fabbc9dd5d6427237095320aca6
hatwell <caroline.hatwell@gmail.com>
2017-03-17 16:21:16 +0000

    Completed core assignment

---
+++ weekend_homework/songbook.rb
@@ -0,0 +1,13 @@
+# class SongBook
+#   attr_accessor :songs
+#
+#   def initialize
+#     @songs = {
+#       acdc: ["Back In Back", "Thunderstruck"],
+#       neilyoung: ["Old Man", "Heart of Gold"],
+#       drake: ["Controlla", "One Dance"]
+#     }
+#   end
+# end
+#
+# my_songbook = SongBook.new()
--- weekend_homework/specs/room_spec.rb
+++ weekend_homework/specs/room_spec.rb
@@ -1,23 +1,42 @@
 require ('minitest/autorun')
 require ('minitest/emoji')
+require ('pry')
 require_relative ('../lib/room')
 require_relative ('../lib/guest')
 require_relative ('../lib/song')
 
 class RoomTest < Minitest::Test
   def setup
-    @pop_room = Room.new(20)
+    @song_list = [@you_can_call_me_al, @mr_brownstone]
+    @small_room = Room.new(20, @song_list)
     @a_guest = Guest.new()
+    @another_guest = Guest.new()
+    @wichita_lineman = Song.new("Glen Campbell", "Wichita Lineman")
+    @you_can_call_me_al = Song.new("Paul Simon", "You Can Call Me Al")
+    @mr_brownstone = Song.new("Guns N Roses", "Mr Brownstone")
+
   end
 
   def test_room_capacity
-    assert_equal(20, @pop_room.capacity)
+    assert_equal(20, @small_room.capacity)
   end
 
   def test_check_guest_in
-    @pop_room.check_in_guest(@a_guest)
-    assert_equal([@a_guest], @pop_room.guests)
+    @small_room.check_in_guest(@a_guest)
+    assert_equal([@a_guest], @small_room.guests)
   end
+
+  def test_check_guest_out
+    @small_room.guests = [@a_guest, @another_guest]
+    @small_room.check_out_guest(@a_guest)
+    assert_equal(1, @small_room.guests.length)
+  end
+
+  def test_add_song_to_room
+    @small_room.add_song_to_room(@wichita_lineman)
+    assert_equal(@wichita_lineman, @small_room.songs.last)
+  end
+
 
 
 
