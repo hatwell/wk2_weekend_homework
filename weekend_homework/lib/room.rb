adb12e89b6f963fe969ed7aa754ed61899f21e94
hatwell <caroline.hatwell@gmail.com>
2017-03-17 15:18:28 +0000

    created setup variables for room test

--- weekend_homework/lib/room.rb
+++ weekend_homework/lib/room.rb
@@ -1,3 +1,23 @@
+
 class Room
-  
+
+  attr_reader :capacity
+  attr_accessor :guest_count, :guests, :songs
+
+  def initialize(capacity)
+    @capacity = capacity
+    @guest_count = 0
+    @guests = []
+    @songs = songs
+  end
+
+  def check_in_guest(guest)
+    @guests << guest
+  end
+
+  def add_song_to_room(artist, title)
+    new_song = Song.new(artist, title)
+    @songs << new_song
+  end
+
 end
--- weekend_homework/lib/song.rb
+++ weekend_homework/lib/song.rb
@@ -1,3 +1,9 @@
 class Song
-  
+
+  attr_reader :song_title, :artist
+  def initialize(song_title, artist)
+      @song_title = song_title
+      @artist = artist
+  end
+
 end
